c20a3d3c5e1990df2b0e17e9d7bbc5b51a3751eb
Pitt-10 <pedrogutman2010@gmail.com>
2026-08-28 09:10:15 -0300

    falta terminar la rotacion del controller

--- Assets/Scripts/Player/PlayerLook.cs
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -5,10 +5,27 @@ using UnityEngine;
 public class PlayerLook : MonoBehaviour
 {
     [SerializeField] private Transform cameraTransform;
+    [SerializeField] private float lookSensitivity = 0.2f;
+
+    private float cameraPitch;
 
     private GameInput gameInput;
 
     private void Awake() {
         gameInput = GetComponent<GameInput>();
+    }
+
+    private void Update() {
+        Vector2 lookInput = gameInput.GetLookVector();
+
+        float yaw = lookInput.x * lookSensitivity;
+        float pitch = lookInput.y * lookSensitivity;
+
+        transform.Rotate(Vector3.up, yaw);
+
+        cameraPitch -= pitch;
+
+        cameraTransform.localRotation = Quaternion.Euler(cameraPitch, 0f, 0f);
+
     }
 }
