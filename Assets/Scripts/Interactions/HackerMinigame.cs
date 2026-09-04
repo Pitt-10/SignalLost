30ce8a535038a424be58ca87416fd963dcc8574c
Pitt-10 <pedrogutman2010@gmail.com>
2026-09-04 09:08:43 -0300

    ya hice el minijuego del hacker (faltan las pantallas de exito y los textos))

--- Assets/Scripts/Interactions/HackerMinigame.cs
+++ Assets/Scripts/Interactions/HackerMinigame.cs
@@ -7,4 +7,43 @@ public class HackerMinigame : MonoBehaviour
 {
     [SerializeField] private string text;
     [SerializeField] private TMP_Text textDisplay;
+    [SerializeField] private TMP_InputField codeInput;
+
+    private void Start() {
+        textDisplay.text = text;
+
+        codeInput.onValueChanged.AddListener((inputText) => {
+            codeInput.text = inputText.ToUpper();
+        });
+
+        string code = GetCode();
+
+        Debug.Log(code);
+    }
+
+    private string GetCode() {
+        string code = "";
+
+        foreach (char character in text) {
+            if (char.IsUpper(character) || char.IsDigit(character)) { 
+                code += character;
+            }
+        }
+        return code;
+    }
+
+    private string GetPlayerCode() { 
+        return codeInput.text;
+    }
+
+    public void ConfirmCode() { 
+        string correctCode = GetCode();
+        string playerCode = GetPlayerCode();
+
+        if (playerCode == correctCode) {
+            Debug.Log("Correcto");
+        } else {
+            Debug.Log("Incorrecto");
+        }
+    }
 }
