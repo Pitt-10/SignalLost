9cce6fbfd122a2467422662085307a3130e52c1a
Pitt-10 <pedrogutman2010@gmail.com>
2026-09-18 12:14:44 -0300

    seguir con lo de trepar

--- Assets/Scripts/Interactions/ExplorerAbilities.cs
+++ Assets/Scripts/Interactions/ExplorerAbilities.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class ExplorerAbilities : MonoBehaviour
 {
-    
+    [SerializeField] private float climbCheckDistance = 1.5f;
+    [SerializeField] private LayerMask climbableLayer;
+    private bool CanClimb(){
+        return Physics.Raycast(transform.position, transform.forward, climbCheckDistance, climbableLayer);
+    }
+
+    private void Update() {
+        if (CanClimb()) {
+            Debug.Log("Esta pared se puede trepar");
+        }
+    }
 }
